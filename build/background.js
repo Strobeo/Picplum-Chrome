e8bf2185cd4927f6902cf33c6175156563d00b65
Akshay Dodeja <dodeja@gmail.com>
2012-05-06 22:31:48 -0700

    PRINT BUTTON FOR THE WEB- HUUZZAAAAAAHHHH

--- build/background.js
+++ build/background.js
@@ -3,7 +3,8 @@
 // found in the LICENSE file.
 
 var plum = {
-  api_base: "https://www.picplum.com/api/1/"
+  // api_base: "https://www.picplum.com/api/1/"
+  api_base: "http://dodeja.mine.nu:3000/api/1/"
 }
 
 
@@ -20,25 +21,81 @@ function checkForValidUrl(tabId, changeInfo, tab) {
 function  add_photo(tab){
   
   
-  $.get(plum.api_base+'user', function(data) {
-    var notification = webkitNotifications.createNotification(
-      'icon1.png',  // icon url - can be relative
-      'Photo added to Picplum',  // notification title
-      data.name  // notification body text
-    );
-
-    // Then show the notification.
-    notification.show();
-    window.setInterval(function() {
-      notification.cancel();
-    }, 3500);
+  $.post(plum.api_base+'photos', 
+    { image_url: plum.dl_url, via: 'web'},
+    function(data) {
+      var notification = webkitNotifications.createNotification(
+        plum.dl_url,
+        'Photo added to Picplum'
+      );
+  
+      // Then show the notification.
+      notification.show();
+      window.setInterval(function() {
+        notification.cancel();
+      }, 3500);
   });
   
-  console.log($('.fbPhotosPhotoActionsItem'))
+
+  // $.get(plum.api_base+'user', function(data) {
+  //   var notification = webkitNotifications.createNotification(
+  //     'icon1.png',  // icon url - can be relative
+  //     'Photo added to Picplum',  // notification title
+  //     data.name  // notification body text
+  //   );
+  // 
+  //   // Then show the notification.
+  //   notification.show();
+  //   window.setInterval(function() {
+  //     notification.cancel();
+  //   }, 3500);
+  // });
+  // 
+  // console.log(plum.dl_url)
 }
 
+// $.post('https://www.picplum.com/api/1/photos', 
+//   { image_url: 'https://fbcdn-sphotos-a.akamaihd.net/hphotos-ak-ash3/582033_747992485321_42002206_34075714_1838519744_n.jpg', via: 'fb_send'}
+// );
 
 
-// Listen for any changes to the URL of any tab.
+// $.post(plum.api_base+'photos', 
+//   { image_url: plum.dl_url, via: 'fb_send'},
+//   function(data) {
+//     var notification = webkitNotifications.createNotification(
+//       'icon1.png',  // icon url - can be relative
+//       'Photo added to Picplum'
+//     );
+// 
+//     // Then show the notification.
+//     notification.show();
+//     window.setInterval(function() {
+//       notification.cancel();
+//     }, 3500);
+// });
+
+function onRequest(request, sender, sendResponse) {
+  // Show the page action for the tab that the sender (content script) was on.
+  chrome.pageAction.show(sender.tab.id);
+
+  plum.dl_url = request.href;
+  console.log(request.href)
+
+  // Return nothing to let the connection be cleaned up.
+  sendResponse({});
+};
+
+// Listen for the content script to send a message to the background page.
+
+
+// Event Listeners 
+
+// When tab is updated (loaded)
 chrome.tabs.onUpdated.addListener(checkForValidUrl);
+
+// When page action is clicked (add photo)
 chrome.pageAction.onClicked.addListener(add_photo);
+
+// Message recieved from tab - Download url for Facebook imaage
+chrome.extension.onRequest.addListener(onRequest);
+
--- build/contentscript.js
+++ build/contentscript.js
@@ -8,7 +8,29 @@ var regex = /sandwich/;
 // Test the text of the body element against our regular expression.
 if (regex.test(document.body.innerText)) {
   // The regular expression produced a match, so notify the background page.
-  chrome.extension.sendRequest({}, function(response) {});
 } else {
   // No match was found.
 }
+
+function check_downloadlink(){
+  $('.fbPhotosPhotoActionsItem').each(function(i, el){
+    var this_el = $(el);
+    if($.trim(this_el.text()) === "Download"){
+      chrome.extension.sendRequest({msg: 'facebook_image', href: this_el.attr('href')}, function(response) {});
+    }
+
+  });
+}
+
+check_downloadlink();
+
+var lasthash = window.location.href;
+setInterval(function(){
+  var currenthash = window.location.href;
+  if(lasthash != currenthash){
+    check_downloadlink();
+    console.log('url change')
+    lasthash = currenthash;
+  }
+}, 500);//check every half second if the url has changed
+
